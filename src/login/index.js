7630e0e05f7b3aad30af5fcc95ef3d3b678f9e61
123wuyujuan <1144714127@qq.com>
2020-06-01 10:42:09 +0800

    第三次提交

--- src/login/index.js
+++ src/login/index.js
@@ -5,13 +5,27 @@ import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 
-export const LoginForm = () => {
+export const LoginForm = (props) => {
     const onFinish = values => {
       console.log('Received values of form: ', values);
+      const usr=values.username.trim();
+      const pwd=values.password.trim();
+      if(usr&&pwd) {
+        console.log(usr,pwd)
+        alert('发送ajax请求');
+
+      }
     };
+    
+    /*const handleSubmit=e=>{
+      e.preventDefault();
+      props.form.validateFields((err,values)=>{
+        if(!err){
+          console.log("Received value of form:",values);
+        }
+      });
+    };*/
     return (
-      
-      
       <Form
         name="normal_login"
         className="login-form"
@@ -19,15 +33,34 @@ export const LoginForm = () => {
           remember: true,
         }}
         onFinish={onFinish}
+        
       >
-          <h1 id="h1">用户登录</h1>
+        <h1 id="h1">用户登录</h1>
         <Form.Item
           name="username"
+          
           rules={[
             {
               required: true,
               message: 'Please input your Username!',
             },
+            {
+              whitespace:true
+            },
+            {
+              min:4,
+              message:'用户名应大于4位 '
+            },
+            {
+              max:16,
+              message:'用户名应小于16位'
+            }/* ,
+            {
+              pattern:/^[a-zA-Z0-9_]+$/,
+              message:'用户名应为字母、数字或下划线组成'
+            } */
+
+
           ]}
         >
           <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
@@ -39,6 +72,14 @@ export const LoginForm = () => {
               required: true,
               message: 'Please input your Password!',
             },
+            {
+              min:6,
+              message:'密码长度不能小于6'
+            },
+            {
+              pattern:/^[a-zA-Z0-9_]+$/,
+              message:'密码应为字母、数字或下划线组成'
+            }
           ]}
         >
           <Input
@@ -56,7 +97,6 @@ export const LoginForm = () => {
             忘记密码
           </a>
         </Form.Item>
-  
         <Form.Item>
           <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
@@ -75,13 +115,9 @@ export default class Login extends Component{
                   <img src={logo} alt="logo"/>
                   <h1> 后台管理系统 </h1>
               </div>
-              <div className="login-content">
-
-                  
+              <div className="login-content"> 
                   <LoginForm></LoginForm>
               </div>
-
-
             </div>
 
         )
